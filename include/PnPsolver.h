ad536ba7982fdc5bd97fa79224debd2780ef7ccd
Patrick Stoebenau <pstoebenauwps@gmail.com>
2022-02-19 16:50:13 -0500

    Fixed OpenCV4 problems

--- include/PnPsolver.h
+++ include/PnPsolver.h
@@ -52,6 +52,8 @@
 #define PNPSOLVER_H
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/core/core_c.h>
 #include "MapPoint.h"
 #include "Frame.h"
 
--- src/FrameDrawer.cc
+++ src/FrameDrawer.cc
@@ -21,6 +21,7 @@
 #include "FrameDrawer.h"
 #include "Tracking.h"
 
+#include <opencv2/imgproc/types_c.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
